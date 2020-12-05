559d49ee9fe91892dffdc3ba2f838edea167d23b
domnantas <domantas@zyro.com>
2020-12-05 23:42:29 +0200

    Add homepage

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+const HomePage = () => {
+  return (
+    <>
+      <h1>HomePage</h1>
+      <p>My name:</p>
+      <p>My kaledaitis id:</p>
+      <p>This kaledaitis is shared with:</p>
+      <p>I have taken kaledaitis from:</p>
+    </>
+  )
+}
+
+export default HomePage
