92c1ed23423225fe8e9fc64182cfed46ce6e541b
domnantas <domantas@zyro.com>
2020-12-23 03:17:12 +0200

    Sigh

--- web/src/components/Kaledaitis/Kaledaitis.js
+++ web/src/components/Kaledaitis/Kaledaitis.js
@@ -1,23 +1,33 @@
 import { useRef } from 'react'
-import { Canvas, useFrame, useLoader } from 'react-three-fiber'
+import { Canvas, useLoader } from 'react-three-fiber'
 import * as THREE from 'three'
+import { useSpring, animated } from 'react-spring/three'
+import { useMove } from 'react-use-gesture'
 import './Kaledaitis.css'
 
 import displacement from 'src/assets/displacement.png'
 import normal from 'src/assets/normal.png'
 import { Suspense } from 'react'
-import { Box } from 'drei'
 
 const KaledaitisBox = () => {
   const displacementTexture = useLoader(THREE.TextureLoader, displacement)
   const normalTexture = useLoader(THREE.TextureLoader, normal)
 
   const mesh = useRef()
-  useFrame(() => {
-    mesh.current.rotation.x = mesh.current.rotation.y += 0.01
+  const [props, set] = useSpring(() => ({
+    rotation: [0, 0, 0],
+    config: { mass: 5, tension: 350, friction: 40 },
+  }))
+
+  const bind = useMove(({ xy: [px, py] }) => {
+    const xPush = (px - window.innerWidth / 2) / 1000
+    const yPush = (py - window.innerHeight / 2) / 1000
+    set({ rotation: [yPush, xPush, 0] })
   })
+
   return (
-    <Box ref={mesh} args={[3, 4, 0.08, 1024, 1024]}>
+    <animated.mesh {...bind()} rotation={props.rotation} ref={mesh}>
+      <boxBufferGeometry args={[3, 4, 0.08, 1024, 1024]} />
       <meshStandardMaterial attachArray="material" color="pink" />
       <meshStandardMaterial attachArray="material" color="pink" />
       <meshStandardMaterial attachArray="material" color="pink" />
@@ -30,7 +40,7 @@ const KaledaitisBox = () => {
         normalMap={normalTexture}
       />
       <meshStandardMaterial attachArray="material" color="pink" />
-    </Box>
+    </animated.mesh>
   )
 }
 
