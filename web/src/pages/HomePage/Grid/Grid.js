475e90493d1baa5b99386a4aa7c952938671e831
domnantas <domantas@zyro.com>
2020-12-24 02:09:22 +0200

    Add optiions

--- web/src/pages/HomePage/Grid/Grid.js
+++ web/src/pages/HomePage/Grid/Grid.js
@@ -6,18 +6,23 @@ const Grid = (props) => {
   const kaledaitisRef = useRef()
 
   useEffect(() => {
-    VanillaTilt.init(kaledaitisRef.current)
+    VanillaTilt.init(kaledaitisRef.current, {
+      gyroscope: true,
+      'full-page-listening': true,
+      speed: 600,
+    })
   })
 
   return (
     <div className="Grid">
       {props.children[0]}
-      <img
-        src="src/assets/kaledaitis.png"
-        ref={kaledaitisRef}
-        onClick={props.cycleStages}
-        style={{ maxHeight: '70vh' }}
-      />
+      <div ref={kaledaitisRef}>
+        <img
+          src="src/assets/kaledaitis.png"
+          onClick={props.cycleStages}
+          style={{ maxHeight: '70vh' }}
+        />
+      </div>
       {/* <Kaledaitis /> */}
       {props.children[1]}
     </div>
